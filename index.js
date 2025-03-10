17b7f1476f344410c71e507b5ede106ef84389aa
Tany <147582125+tanyamedina@users.noreply.github.com>
2025-03-10 11:33:34 -0700

    Update index.js
    Cambio de mensaje de hola

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hola desde Heroku!\n');
+    res.end('Configurando el entorno de desarrollo para desplegar una app y probar Ephimereral Environments proximamente. Proyecto para UDG. Tanya Medina\n');
 });
 server.listen(port, () => {
     console.log(`Servidor corriendo en puerto ${port}`);
